808559964f163f33110f8f1bd65448adb4ef613e
jann <jjung5459@gmail.com>
2022-07-16 22:43:30 +0900

    Adapter pattern

---
+++ adapter/SocketAdapter.java
@@ -0,0 +1,16 @@
+package adapter;
+
+public class SocketAdapter implements Electronic110V {
+
+    private Electronic220V electronic220V;
+
+    public SocketAdapter(Electronic220V electronic220V) {
+        this.electronic220V = electronic220V;
+    }
+
+    @Override
+    public void poweron() {
+        electronic220V.connect();
+
+    }
+}
