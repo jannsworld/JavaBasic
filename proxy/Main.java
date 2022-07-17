63166815e47379773123a6b2c4ef50d9d86ee24d
jann <jjung5459@gmail.com>
2022-07-17 23:35:02 +0900

    Proxy pattern -ing

---
+++ proxy/Main.java
@@ -0,0 +1,12 @@
+package proxy;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Browser browser = new Browser("www.naver.com");
+        browser.show();
+        
+    }
+
+}
+
