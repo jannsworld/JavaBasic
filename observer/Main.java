4257b623d013af10cb4dc878b6ea9b4430a41b43
jann <jjung5459@gmail.com>
2022-07-19 23:36:27 +0900

    Observer pattern

---
+++ observer/Main.java
@@ -0,0 +1,21 @@
+package observer;
+
+public class Main {
+    public static void main(String[] args) {
+        Button button = new Button("버튼");
+
+        button.addListener(new IButtonListener() {
+            @Override
+            public void clickEvent(String event) {
+                System.out.println(event);
+            }
+        });
+
+        button.click("메세지 전달 : click1");
+        button.click("메세지 전달 : click2");
+        button.click("메세지 전달 : click3");
+
+    }
+
+}
+
